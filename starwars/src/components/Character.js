44d83e03857558fb948aded0b5268fcddffc186c
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-02-15 08:26:09 -0800

    Imported Character Component and passed a individual character as props to the Character Component

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Character = props => {
     return (
-        <div></div>;
+        <div>{props.character}</div>
     )
 }
 
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,11 +1,14 @@
 import React from 'react';
+
+import Character from './Character';
+
 import './StarWars.css';
 
 
 const CharacterList = props => {
     return (
         <div>{props.characters.map(character => (
-            character.name
+            <Character character={character} />
         ))}</div>
     )
 }
